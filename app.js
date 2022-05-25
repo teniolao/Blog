af2929aca4e9a85cebc80a5de1649d4a9ad6c97d
teniolao <Tennybetty100@gmail.com>
2022-05-25 16:36:35 +0100

    get new post using params

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,8 +19,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const posts = [];
 app.get("/", (req, res) => {
-  res.render("home", { content: homeStartingContent });
+  res.render("home", { content: homeStartingContent, posts: posts });
+  //console.log(posts)
 });
 
 app.get("/about", (req, res) => {
@@ -28,6 +31,36 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.render("contact", { content: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const postTitle = req.body.postTitle;
+  const postBody = req.body.postBody;
+  const post = {
+    title: postTitle,
+    content: postBody,
+  };
+  posts.push(post);
+
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", (req, res) => {
+  let requestedTitle = req.params.postName;
+  requestedTitle = lodash.lowerCase(requestedTitle);
+  posts.map((post) => {
+    let storedTitle = post.title.toLowerCase();
+    if (requestedTitle == storedTitle) {
+      //console.log("Match found!");
+      res.render("post", { title: post.title, content: post.content });
+    } else {
+      console.log("Not a match!");
+    }
+  });
 });
 
 app.listen(3000, function () {
